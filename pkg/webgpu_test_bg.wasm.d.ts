558405d1aac78404833c668affc0d7d5d2c003c9
Mario Velez <mvlego02@gmail.com>
2026-01-14 00:31:01 -0800

    added pkg directory

---
+++ pkg/webgpu_test_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __wasm_bindgen_func_elem_1555: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_1554: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_741: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_428: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_746: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_739: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
