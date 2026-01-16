9fbfa130b9b9bc908450077bee7814eb5b273b38
Mario Velez <mvlego02@gmail.com>
2026-01-16 15:05:30 -0800

    Camera uniform
    Created a camera class that updates a uniform

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod texture;
+mod camera;
 
 use std::sync::Arc;
 
@@ -66,6 +67,11 @@ pub struct State {
 	num_indices: u32,
 	diffuse_bind_group: wgpu::BindGroup,
 	diffuse_texture: texture::Texture,
+	camera: camera::Camera,
+	camera_uniform: camera::CameraUniform,
+	camera_buffer: wgpu::Buffer,
+	camera_bind_group: wgpu::BindGroup,
+	camera_controller: camera::CameraController,
 }
 
 impl State {
@@ -157,6 +163,56 @@ impl State {
 			}
 		);
 
+		let camera = camera::Camera {
+			eye: (0.0, 1.0, 2.0).into(),
+			target: (0.0, 0.0, 0.0).into(),
+			up: cgmath::Vector3::unit_y(),
+			aspect: config.width as f32 / config.height as f32,
+			fovy: 45.0,
+			znear: 0.1,
+			zfar: 100.0,
+		};
+
+		let mut camera_uniform = camera::CameraUniform::new();
+		camera_uniform.update_view_proj(&camera);
+
+		let camera_buffer = device.create_buffer_init(
+			&wgpu::util::BufferInitDescriptor {
+				label: Some("Camera Buffer"),
+				contents: bytemuck::cast_slice(&[camera_uniform]),
+				usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+			}
+		);
+
+		let camera_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+			entries: &[
+				wgpu::BindGroupLayoutEntry {
+					binding: 0,
+					visibility: wgpu::ShaderStages::VERTEX,
+					ty: wgpu::BindingType::Buffer {
+						ty: wgpu::BufferBindingType::Uniform,
+						has_dynamic_offset: false,
+						min_binding_size: None,
+					},
+					count: None,
+				}
+			],
+			label: Some("camera_bind_group_layout"),
+		});
+
+		let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+			layout: &camera_bind_group_layout,
+			entries: &[
+				wgpu::BindGroupEntry {
+					binding: 0,
+					resource: camera_buffer.as_entire_binding(),
+				}
+			],
+			label: Some("camera_bind_group"),
+		});
+
+		let camera_controller = camera::CameraController::new(0.2);
+
 		let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
 			label: Some("Shader"),
 			source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
@@ -164,7 +220,10 @@ impl State {
 
 		let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
 			label: Some("Render Pipeline Layout"),
-			bind_group_layouts: &[&texture_bind_group_layout],
+			bind_group_layouts: &[
+				&texture_bind_group_layout,
+				&camera_bind_group_layout,
+			],
 			immediate_size: 0,
 		});
 
@@ -239,6 +298,11 @@ impl State {
 			num_indices,
 			diffuse_bind_group,
 			diffuse_texture,
+			camera,
+			camera_uniform,
+			camera_buffer,
+			camera_bind_group,
+			camera_controller,
 		})
 	}
 
@@ -251,15 +315,18 @@ impl State {
 		}
 	}
 
-	pub fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
-		match (code, is_pressed) {
-			(KeyCode::Escape, true) => event_loop.exit(),
-			_ => {}
+	pub fn handle_key(&mut self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+		if code == KeyCode::Escape && is_pressed {
+			event_loop.exit();
+		} else {
+			self.camera_controller.handle_key(code, is_pressed);
 		}
 	}
 
 	fn update(&mut self) {
-
+		self.camera_controller.update_camera(&mut self.camera);
+		self.camera_uniform.update_view_proj(&self.camera);
+		self.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
 	}
 
 	pub fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
@@ -302,6 +369,8 @@ impl State {
 
 			render_pass.set_pipeline(&self.render_pipeline);
 			render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
+
+			render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
 			render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
 			render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
 
@@ -419,7 +488,7 @@ impl ApplicationHandler<State> for App {
 					},
 					..
 			} => state.handle_key(event_loop, code, key_state.is_pressed()),
-			_ => {}
+			_ => {},
 		}
 	}
 }
