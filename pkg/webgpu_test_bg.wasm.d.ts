de79ef41a7900ea3e1da8c75f057a9273cc75b98
Mario Velez <mvlego02@gmail.com>
2026-01-16 15:35:49 -0800

    update

--- pkg/webgpu_test_bg.wasm.d.ts
+++ pkg/webgpu_test_bg.wasm.d.ts
@@ -2,12 +2,12 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export const run_web: () => void;
-export const __wasm_bindgen_func_elem_7569: (a: number, b: number, c: number) => void;
-export const __wasm_bindgen_func_elem_7256: (a: number, b: number) => void;
 export const __wasm_bindgen_func_elem_9202: (a: number, b: number, c: number) => void;
 export const __wasm_bindgen_func_elem_9201: (a: number, b: number) => void;
-export const __wasm_bindgen_func_elem_7574: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_7569: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_7256: (a: number, b: number) => void;
 export const __wasm_bindgen_func_elem_7567: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_7574: (a: number, b: number, c: number, d: number) => void;
 export const __wbindgen_export: (a: number, b: number) => number;
 export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
 export const __wbindgen_export3: (a: number) => void;
